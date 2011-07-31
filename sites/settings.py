840103b72ccda279f4250ddbd8dbf5e616416b11
vicalloy <zbirder@gmail.com>
2011-07-31 22:09:21 +0800

    * add templates path templates_plus

--- sites/settings.py
+++ sites/settings.py
@@ -120,6 +120,7 @@ ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     os.path.join(HERE, 'templates'),
+    os.path.join(HERE, 'templates_plus'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
